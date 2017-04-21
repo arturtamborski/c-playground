8d2df760cd6b87f6cffa58b1fbf71addca23bf03
Artur Tamborski <tamborskiartur@gmail.com>
2017-04-21 17:27:10 +0200

    add stupid-grep

---
+++ stupid-grep/src/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define LINE_MAX 1000
+
+int
+getline(char *str, int len)
+{
+    int c, i = 0;
+
+    while(--len > 0 && (c = getchar()) != EOF && c != '\n')
+        str[i++] = c;
+
+    if (c == '\n')
+        str[i++] = c;
+
+    str[i] = '\0';
+    return i;
+}
+
+int
+strindex(char *str, char *pattern)
+{
+    int i, j, k;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (j = i, k = 0; pattern[k] != '\0' && str[j] == pattern[k]; j++, k++)
+            ;
+        if (k > 0 && pattern[k] == '\0')
+            return i;
+    }
+    return -1;
+}
+
+int
+main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        puts(
+            "./stupidgrep <file> <pattern>\n"
+            "\n"
+            "   prints lines matching <pattern> in <file>\n"
+        );
+        return 1;
+    }
+
+    char *pattern = argv[2];
+    char line[LINE_MAX] = {};
+
+    if (!freopen(argv[1], "r", stdin))
+        return 2;
+
+    while (getline(line, LINE_MAX) > 0)
+    {
+        if (strindex(line, pattern) >= 0)
+            puts(line);
+    }
+}
