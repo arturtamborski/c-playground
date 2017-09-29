774b5181ff7673b12998095c3cc6f38d44c3251f
Artur Tamborski <tamborskiartur@gmail.com>
2017-09-29 20:27:02 +0200

    add stupid-ls

---
+++ stupid-ls/src/main.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+
+int
+main (int argc, char **argv)
+{
+	DIR *dir;
+	struct dirent *ent;
+
+	if (argc < 2)
+		return (-1);
+
+	if ((dir = opendir(argv[1])))
+	{
+		while ((ent = readdir(dir)) != NULL)
+		{
+			// i should use printf and print
+			// some more info about those files
+			puts(ent->d_name);
+		}
+	}
+
+	return (0);
+}
